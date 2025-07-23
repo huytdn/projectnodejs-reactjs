33b9fa19bc027265a4a2b1dc62cce6c54ecae798
huytdn <saolailatoivay991@gmail.com>
2025-07-23 16:19:22 +0700

    create new user

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,3 +1,11 @@
+import mysql from "mysql2";
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  database: "jwtnodejs+reactjs",
+});
+
 const handleHelloWorld = (req, res) => {
   res.render("home.ejs");
 };
@@ -6,4 +14,21 @@ const handleUserPage = (req, res) => {
   res.render("user.ejs");
 };
 
-export default { handleHelloWorld, handleUserPage };
+const handleCreateNewUser = (req, res) => {
+  let email = req.body.email;
+  let password = req.body.password;
+  let username = req.body.username;
+
+  connection.query(
+    "  INSERT INTO Users (email, password, username) VALUES (?, ?, ?)",
+    [email, password, username],
+    function (err, results, fields) {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
+  res.send("handleCreateNewUser");
+};
+
+export default { handleHelloWorld, handleUserPage, handleCreateNewUser };
--- src/server.js
+++ src/server.js
@@ -2,9 +2,12 @@ import express from "express";
 import configViewEngine from "./configs/viewEngine";
 import initWebRoutes from "./routes/web";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
 dotenv.config();
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 configViewEngine(app);
 initWebRoutes(app);
