3216a98f77cc22ca071ac5dda5de6d6bd9ee5eff
huytdn <saolailatoivay991@gmail.com>
2025-08-19 21:27:51 +0700

    test api

--- src/models/user.js
+++ src/models/user.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.belongsTo(models.Group);
-      Group.belongsToMany(models.Project, { through: "Project_User" });
+      User.belongsToMany(models.Project, { through: "Project_User" });
     }
   }
   User.init(
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,5 +1,7 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import apiController from "../controllers/apiController";
+
 const router = express.Router();
 
 const initWebRoutes = (app) => {
@@ -9,6 +11,10 @@ const initWebRoutes = (app) => {
   router.post("/delete-user/:id", homeController.handleDeleteUser);
   router.get("/update-user/:id", homeController.handleUpdatePage);
   router.post("/users/update-user", homeController.handleUpdateUser);
+
+  // rest api
+
+  router.get("/api/test-api", apiController.testApi);
 
   return app.use("/", router);
 };
